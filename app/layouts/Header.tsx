977db3d2fbf932c7b3c07b824e539acd29e3be26
Satoki Hata <satokihata@gmail.com>
2024-04-22 23:23:44 +0900

    add component

--- app/layouts/Header.tsx
+++ app/layouts/Header.tsx
@@ -1,5 +1,12 @@
 import React from "react";
+import { Account } from "../components/Account/Account";
+import { Login } from "../components/Login/Login";
 
 export const Header = () => {
-	return <div>Header</div>;
+	return (
+		<div>
+			<Login />
+			<Account />
+		</div>
+	);
 };
