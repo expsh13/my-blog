8d7cefc5b5d2a7caca4745006c2acd6eb0d480e6
Satoki Hata <satokihata@gmail.com>
2024-04-24 19:55:41 +0900

    add zod

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,11 +1,13 @@
+import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
+import { z } from "zod";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
-app.get("/hello", (c) => {
+app.get("/hello", zValidator("query", z.ob), (c) => {
 	return c.json({
 		message: "Hello Next.js!",
 	});
